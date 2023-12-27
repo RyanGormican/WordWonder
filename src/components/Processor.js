e104e43eb2ba8c7abe2f74e9eed7ffedf7e34966
RyanGormican <ryan.gormican@gmail.com>
2023-12-27 04:52:50 -0600

    a
    a

--- src/components/Processor.js
+++ src/components/Processor.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Editor, EditorState, convertToRaw  } from 'draft-js';
+import { Editor, EditorState, convertToRaw, RichUtils   } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { Icon } from '@iconify/react';
 import html2pdf from 'html2pdf.js';
@@ -7,6 +7,22 @@ const Processor = () => {
 const editorRef = useRef(null);
 const [words,setWords] = useState(0);
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const italicizeText = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'ITALIC'));
+  } 
+  const boldText = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+  };
+    const underlineText = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+  };
+    const countWords = () => {
+    const contentState = editorState.getCurrentContent();
+    const plainText = contentState.getPlainText();
+    const wordCount = plainText.split(/\s+/).filter((word) => word.length > 0).length;
+    setWords(wordCount);
+  };
+
   const downloadDocument = () => {
     const plainText = editorState.getCurrentContent().getPlainText();
     const pdfElement = document.createElement('div');
@@ -21,18 +37,26 @@ const [words,setWords] = useState(0);
   return (
     <div className="processor">
     <span className="activity">
+        <button onClick={italicizeText}>
+    <Icon icon="ooui:italic-a" height="30" />
+        </button>
+          <button onClick={boldText}>
+    <Icon icon="ooui:bold-a" height="30" />
+    </button>
+       <button onClick={underlineText}>
+    <Icon icon="majesticons:underline-2" height="30" />
+    </button>
     <button onClick={downloadDocument}>
     <Icon icon="material-symbols:download" height="30"/>
     </button>
-    {words > 1 || words == 0 ? (
-    {words} words
-    ) : ( 
-    {words} word
-    )}
+  {words === 0 || words > 1 ? `${words} words` : `${words} word`}
     </span>
        <Editor
         editorState={editorState}
-       onChange={setEditorState}
+      onChange={(newEditorState) => {
+          setEditorState(newEditorState);
+          countWords();
+        }}
       wrapperClassName="processor-wrapper"
         editorClassName="processor-editor"
        />
