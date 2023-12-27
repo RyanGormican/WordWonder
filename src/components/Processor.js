12af6d651ff1eeb757e723775f0df43af6990ea2
RyanGormican <ryan.gormican@gmail.com>
2023-12-27 04:26:05 -0600

    a
    a

--- src/components/Processor.js
+++ src/components/Processor.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { Editor, EditorState } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
+import { Icon } from '@iconify/react';
 const Processor = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
   return (
     <div className="processor">
+    <Icon icon="material-symbols:download" />
       <Editor
-      className="processor"
         editorState={editorState}
        onChange={setEditorState}
-      />
+      wrapperClassName="processor-wrapper"
+        editorClassName="processor-editor"
+       />
     </div>
   );
 };
