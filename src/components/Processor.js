7f93db10ce927ce2a5993f7a38fdee3c8a81bc0b
RyanGormican <ryan.gormican@gmail.com>
2023-12-27 04:38:13 -0600

    a
    a

--- src/components/Processor.js
+++ src/components/Processor.js
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import React, { useState, useRef } from 'react';
+import { Editor, EditorState, convertToRaw  } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { Icon } from '@iconify/react';
+import html2pdf from 'html2pdf.js';
 const Processor = () => {
+const editorRef = useRef(null);
+const [words,setWords] = useState(0);
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const downloadDocument = () => {
+    const plainText = editorState.getCurrentContent().getPlainText();
+    const pdfElement = document.createElement('div');
+    pdfElement.innerText = plainText;
 
+    html2pdf(pdfElement, {
+      margin: 10,
+      filename: 'document.pdf',
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    });
+  };
   return (
     <div className="processor">
-    <Icon icon="material-symbols:download" />
-      <Editor
+    <span className="activity">
+    <button onClick={downloadDocument}>
+    <Icon icon="material-symbols:download" height="30"/>
+    </button>
+    {words > 1 || words == 0 ? (
+    {words} words
+    ) : ( 
+    {words} word
+    )}
+    </span>
+       <Editor
         editorState={editorState}
        onChange={setEditorState}
       wrapperClassName="processor-wrapper"
